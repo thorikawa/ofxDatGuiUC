c7742e1a747cf0e5af73b54899bfc62497f39afd
Takahiro Poly Horikawa <horikawa.takahiro@gmail.com>
2019-01-27 01:20:14 +0900

    Use utf8 string as default.

--- src/libs/ofxTrueTypeFontUC/ofxTrueTypeFontUC.cpp
+++ src/libs/ofxTrueTypeFontUC/ofxTrueTypeFontUC.cpp
@@ -40,20 +40,10 @@ static const basic_string<unsigned int> convToUTF32(const string &src) {
     if (src.size() == 0) {
         return basic_string<unsigned int> ();
     }
-    
-    // convert XXX -> UTF-16
-    const int n_size = ::MultiByteToWideChar(CP_ACP, 0, src.c_str(), -1, NULL, 0);
-    vector<wchar_t> buffUTF16(n_size);
-    ::MultiByteToWideChar(CP_ACP, 0, src.c_str(), -1, &buffUTF16[0], n_size);
-    
-    // convert UTF-16 -> UTF-8
-    const int utf8str_size = ::WideCharToMultiByte(CP_UTF8, 0, &buffUTF16[0], -1, NULL, 0, NULL, 0);
-    vector<char> buffUTF8(utf8str_size);
-    ::WideCharToMultiByte(CP_UTF8, 0, &buffUTF16[0], -1, &buffUTF8[0], utf8str_size, NULL, 0);
-    
+
     // convert UTF-8 -> UTF-32 (UCS-4)
     std::wstring_convert<std::codecvt_utf8<unsigned int>, unsigned int> convert32;
-    return convert32.from_bytes(&buffUTF8[0]);
+    return convert32.from_bytes(src);
 }
 
 #else
